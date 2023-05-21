8196616e2690e8bb898cf14482284e73ed570161
Akhil K A <akhilkajayaghosh@gmail.com>
2023-05-21 07:38:20 +0530

    Python Source code

--- youtube-video-downloader.py
+++ youtube-video-downloader.py
@@ -4,8 +4,9 @@ from tkinter import filedialog
 
 window=tk.Tk()
 window.geometry("800x250")
-window.title("Youtube Video Downloader")
+window.title("Youtube Downloader")
 window.configure(bg="#BEBEBE")
+window.resizable(False,False)
 def download(url,op):
     try:
         yt = YouTube(url)
@@ -14,6 +15,7 @@ def download(url,op):
         tk.messagebox.showinfo("showinfo", "Downloaded Successfully")
     except:
         tk.messagebox.showerror("showerror", "Error")
+        
 my_dir=''
 def browse():
     global op
@@ -21,19 +23,19 @@ def browse():
     op.insert(0,my_dir)
 l1=tk.Label(window, text="Paste your Video URL",font=(25))
 l1.place(x=0,y=50)
-url=tk.Entry(window,width=50,font=(40))
+url=tk.Entry(window,width=40,font=(40))
 url.place(x=250,y=50)
 l2=tk.Label(window, text="Select Destination Folder",font=(25))
 l2.place(x=0,y=100)
 
-op=tk.Entry(window,width=50,font=(40),text=my_dir)
+op=tk.Entry(window,width=40,font=(40),text=my_dir)
 op.place(x=250,y=100)
 b2=tk.Button(window, text="Browse",width=8,height=1,command=browse)
-b2.place(x=705,y=100)
+b2.place(x=695,y=101)
 def getdata():
     global url,op
     download(url.get(),op.get())
-b1=tk.Button(window,width=8,height=2,text="SUBMIT",font=(25),command=getdata)
+b1=tk.Button(window,width=11,height=1,text="DOWNLOAD",font=(25),command=getdata)
 b1.configure(bg="darkgreen")
 b1.place(x=300,y=150)
 
