638eea3c3d6fd719ed09e375a9265ad556e7fdf1
Akhil K A <akhilkajayaghosh@gmail.com>
2023-05-28 14:48:31 +0530

    Update in functions

--- youtube-video-downloader.py
+++ youtube-video-downloader.py
@@ -8,7 +8,7 @@ window.title("Youtube Downloader")
 window.configure(bg="#BEBEBE")
 window.resizable(False,False)
 lr=tk.Label(window, text="Resolution",font=(25),width=10).place(x=300,y=100)
-lt=tk.Label(window, text="Type",font=(25),width=10).place(x=300,y=130)
+lt=tk.Label(window, text="Format",font=(25),width=10).place(x=300,y=130)
 menu1=StringVar()
 menu2=StringVar()
 menu1.set("select")
@@ -17,33 +17,34 @@ drop.place(x=418,y=100)
 drop= OptionMenu(window, menu2,"select")
 drop.place(x=418,y=130)
 menu2.set("select")
-global yt,rs1,format
-
-def getdetails(url):
-    yt = YouTube(url)
-    res1=[]
+def display_selected(choice):
     format=[]
-    for i in yt.streams.filter(type='video'):
-        res1.append(i.resolution)
+    choice = menu1.get()
+    for i in yt.streams.filter(type='video',res=choice,progressive=True):
         format.append(i.mime_type)
-    res1=(list(set(res1)))
     format=(list(set(format)))
-    drop= OptionMenu(window, menu1,*res1)
-    drop.place(x=420,y=100)
     drop= OptionMenu(window, menu2,*format)
-    drop.place(x=420,y=130)
-    
-def download(url,op):
-    try:
-        yt=YouTube(url)        
-        yt=yt.streams.filter(res=menu1.get(),mime_type=menu2.get(),type='video').first()
-        yt.download(op)
-        tk.messagebox.showinfo("showinfo", "Downloaded Successfully")
-    except:
-        tk.messagebox.showerror("showerror", "Error")
-        
-my_dir=''
+    drop.place(x=420,y=130)    
 
+def getdetails():
+    global yt
+    yt = YouTube(url.get())
+    res1=[]
+    for i in yt.streams.filter(type='video',progressive=True):
+        res1.append(i.resolution)
+    res1=(list(set(res1)))
+    drop= OptionMenu(window,menu1,*res1,command=display_selected)
+    drop.place(x=420,y=100)
+    
+def download():
+    try:
+        yt=YouTube(url.get())
+        yt=yt.streams.filter(res=menu1.get(),mime_type=menu2.get(),type='video').first()
+        yt.download(op.get())
+        status.configure(text="Successfully downloaded")
+    except:
+        status.configure(text="Error!!")
+my_dir=''
 def browse():
     global op
     my_dir= filedialog.askdirectory()
@@ -53,11 +54,6 @@ l1=tk.Label(window, text="Paste your Video URL",font=(25))
 l1.place(x=0,y=50)
 url=tk.Entry(window,width=40,font=(40))
 url.place(x=250,y=50)
-
-def getdata():
-    global url
-    getdetails(url.get())
-  
 l2=tk.Label(window, text="Select Destination Folder",font=(25))
 l2.place(x=0,y=180)
 op=tk.Entry(window,width=40,font=(40),text=my_dir)
@@ -65,15 +61,12 @@ op.place(x=250,y=180)
 b2=tk.Button(window, text="Browse",width=8,height=1,command=browse)
 b2.place(x=695,y=180)
 b2.configure(bg="#C4AB37")
-b3=tk.Button(window, text="Check",width=8,height=1,command=getdata)
+b3=tk.Button(window, text="Enter",width=8,height=1,command=getdetails)
 b3.place(x=695,y=51)
 b3.configure(bg="#C4AB37")
-
-def start():
-    getdetails(url.get())
-    download(url.get(),op.get()) 
-    
-b1=tk.Button(window,width=11,height=1,text="DOWNLOAD",font=(25),command=start)
+status=tk.Label(window, text="",font=(30),bg="#BEBEBE")
+status.place(x=300,y=280)   
+b1=tk.Button(window,width=11,height=1,text="DOWNLOAD",font=(25),command=download)
 b1.configure(bg="darkgreen")
 b1.place(x=300,y=230)
 window.mainloop()
